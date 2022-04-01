8a87e7b18a11223b2ea76545c794b8a9f327daff
Elias <elias.moualem@gmail.com>
2022-04-01 16:34:19 +0200

    tests Class

---
+++ src/test/java/com/employeeLogger/EmployeeLoggerSB/EmployeeLoggerSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeeLogger.EmployeeLoggerSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeLoggerSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
