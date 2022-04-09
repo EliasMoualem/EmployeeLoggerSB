26d2cd240bb68f91ea9f9519f2e3be020870fe65
Elias <elias.moualem@gmail.com>
2022-04-09 22:06:46 +0200

    added delete worker api

--- src/main/java/com/employeeLogger/EmployeeLoggerSB/controller/WorkerController.java
+++ src/main/java/com/employeeLogger/EmployeeLoggerSB/controller/WorkerController.java
@@ -3,28 +3,29 @@ package com.employeeLogger.EmployeeLoggerSB.controller;
 import com.employeeLogger.EmployeeLoggerSB.entity.Worker;
 import com.employeeLogger.EmployeeLoggerSB.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/worker")
 public class WorkerController {
 
     @Autowired
     WorkerRepository workerRepository;
 
-    @GetMapping
+    @RequestMapping(value = "/workers", method = RequestMethod.GET)
     public List<Worker> getAllWorkers(){
         return workerRepository.findAll();
     }
 
-    @GetMapping("/findbyid")
+    @RequestMapping(value = "/findworkerbyid{id}", method = RequestMethod.GET)
     public Optional<Worker> getWorkerByID(@RequestParam Long id){
         return workerRepository.findById(id);
+    }
+
+    @RequestMapping(value = "/deleteworkerbyid{id}", method = {RequestMethod.POST, RequestMethod.GET})
+    public void deleteWorkerByID(@RequestParam Long id){
+        workerRepository.deleteById(id);
     }
 }
